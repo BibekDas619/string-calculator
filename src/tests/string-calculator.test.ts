bcb7bdd3e1189c110ddeb5e9d06c63d0e12ed3a7
BIBEK DAS <bibekdas993@gmail.com>
2025-03-15 13:31:12 +0530

    Written test case for 2 comma-separeted numbers

--- src/tests/string-calculator.test.ts
+++ src/tests/string-calculator.test.ts
@@ -2,14 +2,22 @@ import { describe, expect, it } from "vitest";
 import { StringCalculator } from "../string-calculator";
 
 describe("String Calculator", () => {
-  // Test case for empty string
-  it("should return 0 for empty string", () => {
-    expect(StringCalculator("")).toBe(0);
-  });
+  describe("Basic functionalities", () => {
+    // Test case for empty string
+    it("should return 0 for empty string", () => {
+      expect(StringCalculator("")).toBe(0);
+    });
 
-  // Test case for string with length of 1
-  it("should return the number value for string with single number", () => {
-    expect(StringCalculator("1")).toBe(1);
-    expect(StringCalculator("5")).toBe(5);
+    // Test case for string with length of 1
+    it("should return the number value for string with single number", () => {
+      expect(StringCalculator("1")).toBe(1);
+      expect(StringCalculator("5")).toBe(5);
+    });
+
+    // Test case for 2 comma separated numbers
+    it("should return the sum of 2 comma-separated numbers", () => {
+      expect(StringCalculator("1,5")).toBe(6);
+      expect(StringCalculator("10,20")).toBe(30);
+    });
   });
 });
