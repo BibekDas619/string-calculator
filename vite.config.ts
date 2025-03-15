6df433602b1bddfc014905f85838819d73df4bd4
BIBEK DAS <bibekdas993@gmail.com>
2025-03-15 13:00:12 +0530

    Fixed vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
-    reporters: ["text", "html"],
-    include: ["tests/**/*.test.ts"],
+    includeSource: ["src/tests/**/*.test.ts"],
   },
 });
