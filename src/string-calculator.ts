183d1173ae577618015d7650605c6244f6e7d6f6
BIBEK DAS <bibekdas993@gmail.com>
2025-03-15 16:05:37 +0530

    Refactored code to pass custom delimiter test case

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -5,9 +5,17 @@ export const StringCalculator = (numberString: string): number => {
   }
 
   let delimiter = ",";
+  let numbers = numberString;
+
+  // Check for custom delimiter
+  if (numbers.startsWith("//")) {
+    let newDelimiterEndIndex = numbers.indexOf("\n");
+    delimiter = numbers.substring(2, newDelimiterEndIndex);
+    numbers = numbers.substring(newDelimiterEndIndex + 1);
+  }
 
   // Split the string by the delimiter and convert to numbers
-  let numbersArray: number[] = numberString
+  let numbersArray: number[] = numbers
     .split(delimiter)
     .map((number: string) => parseInt(number, 10));
 
