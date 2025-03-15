39719573530c5367002b2aca196495d0563abf75
BIBEK DAS <bibekdas993@gmail.com>
2025-03-15 12:41:42 +0530

    Written test case for empty string

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -1,3 +1,9 @@
 import { describe, expect, it } from "vitest";
+import { StringCalculator } from "./string-calculator";
 
-describe("String Calculator", () => {});
+describe("String Calculator", () => {
+  // Test case for empty string
+  it("should return 0 for empty string", () => {
+    expect(StringCalculator("")).toBe(0);
+  });
+});
